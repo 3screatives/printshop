fe4588f239425e7e3ecce281a9c1f4812d0d02f6
Sajjad ALi <3screativez@gmail.com>
2025-11-19 23:47:46 -0600

    order-page working with multiple material select dropdown

--- include/footer.php
+++ include/footer.php
@@ -99,6 +99,8 @@
     </div>
 </footer>
 
+<?php include 'cart-slide.php'; ?>
+
 <!-- Scripts -->
 <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.3.2/js/bootstrap.bundle.min.js"></script>
